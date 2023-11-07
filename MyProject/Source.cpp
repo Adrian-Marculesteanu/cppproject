188e3c8f743d260a3efbdc7eee8c92e104950f6d
Adrian Marculesteanu <MarculesteanuAdi@gmail.com>
2023-11-07 18:25:35 +0200

    Started working on the project

--- MyProject/Source.cpp
+++ MyProject/Source.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
+#include <string>
+
+class EventLocation
+{
+	int noSeats;
+	int noRows;
+	int zones;
+	char* locationName;
+public:
+	EventLocation(int seats, int rows, int zone, char* locationName)
+		:noSeats(seats), noRows(rows), zones(zone)
+	{
+		if (noSeats < 0 || noRows < 0 || zones < 0)
+		{
+			throw std::exception("Invalid values");
+		}
+		this->locationName = new char[strlen(locationName) + 1];
+		strcpy_s(this->locationName, strlen(locationName)+1, locationName);
+	}
+
+}
+
+
 int main()
 {
-	std::cout << "Hello World";
+
 }
