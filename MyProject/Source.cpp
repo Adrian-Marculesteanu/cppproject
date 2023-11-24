96d95bc54f51d923dd17ccf660391e8425621fa6
Adrian Marculesteanu <MarculesteanuAdi@gmail.com>
2023-11-24 21:15:38 +0200

    added destructor/constructor

--- MyProject/Source.cpp
+++ MyProject/Source.cpp
@@ -5,7 +5,7 @@ enum class SeatType{NORMAL,SPECIAL_NEEDS,VIP};
 
 class EventDetails
 {
-	const int EventId = 0;
+	const int eventId = 0;
 	char* eventLocation = nullptr;
 	std::string eventName = "";
 	std::string eventDate = "";
@@ -14,8 +14,29 @@ class EventDetails
 
 	static int noEvents;
 public:
+	EventDetails(const int id, char* location, std::string name, std::string date, std::string time, int noParticipants) :eventId(id)
+	{
+		this->eventName = name;
+		this->eventDate = date;
+		this->eventTime = time;
+		this->maxParticipants = noParticipants;
+		if (location != nullptr)
+		{
+			this->eventLocation = new char[strlen(location) + 1];
+			strcpy_s(this->eventLocation, strlen(location) + 1, location);
+		}
+		noEvents++;
+	}
 
+
+
+	~EventDetails()
+	{
+		delete[] this->eventLocation;
+		noEvents--;
+	}
 };
+
 class SeatsDetails
 {
 	int noNormalSeats = 0;
